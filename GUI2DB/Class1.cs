6271775439308632d510041ff85a757994e49a8c
Ewelina <Ewelina@DESKTOP41>
2017-06-07 09:52:34 +0200

    zakomentowane, bo nie mam db4o tutaj

--- GUI2DB/Class1.cs
+++ GUI2DB/Class1.cs
@@ -8,11 +8,35 @@ namespace GUI2DB
 {
     public class Class1
     {
-        public static void WriteToDataBase()
-        {
+        public static void WriteToDataBase(int roomnum)
+        { /*
+            if (addEditContactWindow.DialogResult == true)
+            {
+                Rooms = new List<Room>();
+                room = new Room();
+                IObjectSet result = db.QueryByExample(typeof(Room));
 
+                foreach (var x in result)
+                {
+                    room = (Room)x;
+                    Rooms.Add(new Room
+                    {
+                        reservation = new Reservation
+                        {
+                            Name = reservation.name == null ? "" : reservation.name,
+                            Surname = reservation.surname == null ? "" : reservation.surname,
+                            PESEL = reservation.pesel == null ? "" : reservation.pesel,
+                            Phone = reservation.phone == null ? "" : reservation.phone
+                        },
+                        Name = reservation.name,
+                        Surname = reservation.surname,
+                        PESEL = reservation.pesel,
+                        Phone = reservation.phone
+                    });
+                }
+*/
+            }
+
+          
         }
-            
-
-    }
 }
