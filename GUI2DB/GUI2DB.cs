8c7706181ff167410d4497961dd0c0883850661e
Damian <master0001@wp.pl>
2017-06-13 20:42:29 +0200

    drobne poprawki

--- GUI2DB/GUI2DB.cs
+++ GUI2DB/GUI2DB.cs
@@ -162,9 +162,10 @@ namespace GUI2DB
             config.Common.ObjectClass(typeof(Reservation)).CascadeOnActivate(true);
 
             List<Rooms> Room = new List<Rooms>();
+
             using (IObjectContainer db = Db4oEmbedded.OpenFile(config, path))
             {
-                //Room = (from Rooms r in db select r).ToList().Where(r=>r.Booked==true);
+               //Room = (from Rooms r in db select r).ToList().Where(r=>r.Booked==true);
             }
      
             return Room;
@@ -207,7 +208,7 @@ namespace GUI2DB
             db.Close();
         }
 
-        public static IList<Rooms> GetRoom(int ID)
+        public static Rooms GetRoom(int ID)
         {
             string path = Directory.GetCurrentDirectory() + "\\database.srph";
             IEmbeddedConfiguration config = Db4oEmbedded.NewConfiguration();
@@ -217,8 +218,10 @@ namespace GUI2DB
             config.Common.ObjectClass(typeof(Rooms)).CascadeOnActivate(true);
 
             db = Db4oEmbedded.OpenFile(config, path);
-            var Room = db.Query<Rooms>(x => x.RoomId == ID);
+            var results = db.Query<Rooms>(x => x.RoomId == ID);
+            Rooms Room = results.First();
             return Room;
+
         }
 
         //TODO dodac metodę getRoom dającą dane pokoju po ID do edycji
