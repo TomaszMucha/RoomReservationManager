bee841645eb7d24794af5411b8b89df9add1cfa0
Nira <Nira@Vermiis>
2017-06-26 20:27:37 +0200

    .

--- SRPH-DataBase/BoolStringClass.cs
+++ SRPH-DataBase/BoolStringClass.cs
@@ -8,6 +8,17 @@ namespace SRPH_DataBase
 {
     public class BoolStringClass
     {
+        private object standardName;
+        private object standardPrice;
+        private object isPermamentOrIsNotPermament;
+
+        public BoolStringClass(object standardName, object standardPrice, object isPermamentOrIsNotPermament)
+        {
+            this.standardName = standardName;
+            this.standardPrice = standardPrice;
+            this.isPermamentOrIsNotPermament = isPermamentOrIsNotPermament;
+        }
+
         public string StandardName { get; set; }
         public int StandardPrice { get; set; }
         public bool IsPermamentOrIsNotPermament { get; set; }
