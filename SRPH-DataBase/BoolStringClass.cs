82c4de67bccd7f757ce6759b6026632f4ee8d55d
Tomasz Mucha <TomaszMucha00@gmail.com>
2017-06-27 15:10:26 +0200

    Kolejny restore

--- SRPH-DataBase/BoolStringClass.cs
+++ SRPH-DataBase/BoolStringClass.cs
@@ -4,23 +4,25 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+
 namespace SRPH_DataBase
 {
     public class BoolStringClass
     {
-        private string standardName;
-        private int standardPrice;
-        private bool isPermamentOrIsNotPermament;
-
-        public BoolStringClass(string standardName, int standardPrice, bool isPermamentOrIsNotPermament)
-        {
-            this.standardName = standardName;
-            this.standardPrice = standardPrice;
-            this.isPermamentOrIsNotPermament = isPermamentOrIsNotPermament;
-        }
 
         public string StandardName { get; set; }
         public int StandardPrice { get; set; }
         public bool IsPermamentOrIsNotPermament { get; set; }
+        //private string standardName;
+        //private int standardPrice;
+        //private bool isPermamentOrIsNotPermament;
+
+        public BoolStringClass(string standardName, int standardPrice, bool isPermamentOrIsNotPermament)
+        {
+            StandardName = standardName;
+            StandardPrice = standardPrice;
+            IsPermamentOrIsNotPermament = isPermamentOrIsNotPermament;
+        }
+
     }
 }
